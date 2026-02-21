6987bb0f13670db672309b8ca24a8eab050aac0d
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2026-02-21 12:43:54 +0100

    logout view

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -8,6 +8,12 @@ from api_services.utils import get_tokens_for_user, get_serializer_error
 from django_ratelimit.decorators import ratelimit
 from django.utils.decorators import method_decorator
 from django.utils import timezone
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+import jwt
+from api_services.environmentals import SECRET_KEY
+from api_services.redis_service import RedisService
+from datetime import timedelta
 
 
 # login view
@@ -64,3 +70,72 @@ class RegisterView(APIView):
             get_serializer_error(serializer, "User registration failed"),
             status.HTTP_400_BAD_REQUEST,
         )
+
+
+# logout view
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    # noinspection PyMethodMayBeStatic
+    def post(self, request):
+        auth_header = request.META.get("HTTP_AUTHORIZATION")
+
+        if not auth_header.startswith("Bearer "):
+            return returned_response(
+                "failed",
+                "Invalid token format",
+                status.HTTP_401_UNAUTHORIZED,
+            )
+
+        parts = auth_header.split()
+        if len(parts) != 2:
+            return returned_response(
+                "failed",
+                "Invalid token format.",
+                status.HTTP_401_UNAUTHORIZED,
+            )
+
+        token = parts[1]
+        try:
+            # Decode and verify token
+            payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+
+            # Extract jti
+            jti = payload.get("jti")
+
+            if not jti:
+                return returned_response(
+                    "failed",
+                    "Token does not contain jti claim",
+                    status.HTTP_401_UNAUTHORIZED,
+                )
+
+            redis_service = RedisService()
+
+            has_been_blacklisted = redis_service.get(jti)
+            if has_been_blacklisted:
+                return returned_response(
+                    "failed",
+                    "Token has been blacklisted",
+                    status.HTTP_401_UNAUTHORIZED,
+                )
+
+            redis_service.set(jti, "blacklisted", expire=timedelta(days=1))
+            return returned_response(
+                "success",
+                "Logout successful",
+                status.HTTP_200_OK,
+            )
+        except jwt.ExpiredSignatureError:
+            return returned_response(
+                "failed",
+                "Token has expired",
+                status.HTTP_401_UNAUTHORIZED,
+            )
+        except jwt.InvalidTokenError as e:
+            return returned_response(
+                "failed",
+                f"Invalid token: {str(e)}",
+                status.HTTP_401_UNAUTHORIZED,
+            )
